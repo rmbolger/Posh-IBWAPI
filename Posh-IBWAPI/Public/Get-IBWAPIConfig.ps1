375a6b1430fe260bc78c675858c39b9fcfe17c81
Ryan Bolger <z97y9drumh@snkmail.com>
2017-05-01 11:19:17 -0700

    added -WAPIHost to Get-IBWAPIConfig so you can view a specific host's config other than the current one. Also normalized outputs so all fields (even empty) are shown.

--- Posh-IBWAPI/Public/Get-IBWAPIConfig.ps1
+++ Posh-IBWAPI/Public/Get-IBWAPIConfig.ps1
@@ -2,18 +2,34 @@ function Get-IBWAPIConfig
 {
     [CmdletBinding()]
     param(
+        [string]$WAPIHost,
         [switch]$List
     )
 
     if ($List -and $script:Config) {
         # list all configs
         foreach ($hostConfig in $script:Config.Values) {
-            [PSCustomObject]$hostConfig
+            [PSCustomObject]$hostConfig | Select-Object WAPIHost,WAPIVersion,Credential,WebSession,IgnoreCertificateValidation
+        }
+    }
+    elseif ($script:Config -and $WAPIHost) {
+        if ($script:Config.$WAPIHost) {
+            # show the specified config
+            [PSCustomObject]$script:Config.$WAPIHost | Select-Object WAPIHost,WAPIVersion,Credential,WebSession,IgnoreCertificateValidation
+        } else {
+            # show empty config
+            [PSCustomObject]@{
+                WAPIHost=$null;
+                WAPIVersion=$null;
+                Credential=$null;
+                WebSession=$null;
+                IgnoreCertificateValidation=$null;
+            }
         }
     }
     elseif ($script:Config -and $script:CurrentHost) {
         # show the current config
-        [PSCustomObject]$script:Config.$script:CurrentHost
+        [PSCustomObject]$script:Config.$script:CurrentHost | Select-Object WAPIHost,WAPIVersion,Credential,WebSession,IgnoreCertificateValidation
     }
     else {
         # show empty config
@@ -35,7 +51,12 @@ function Get-IBWAPIConfig
         Get the current set of configuration values for this module.
 
     .DESCRIPTION
-        The configuration values returned by this function will automatically be used by related function calls to the Infoblox API unless they are overridden by the function's own parameters.
+        When calling this function with no parameters, the currently active set of config values will be returned. These values will be used by related function calls to the Infoblox API unless they are overridden by the function's own parameters.
+
+        When called with -WAPIHost, the set of config values for that host will be returned. When called with -List, all sets of config values will be returned.
+
+    .PARAMETER WAPIHost
+        The fully qualified DNS name or IP address of the Infoblox WAPI endpoint (usually the grid master).
 
     .PARAMETER List
         If set, list all config sets currently stored. Otherwise, just list the currently active set.
