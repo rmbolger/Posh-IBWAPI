8d4750c3e7eee19967b3932b58728effbc635ea2
Ryan Bolger <z97y9drumh@snkmail.com>
2017-05-26 14:19:34 -0700

    fix for issue #15 regarding errors when TLS 1.0 is disabled in Infoblox

--- Posh-IBWAPI/Posh-IBWAPI.psm1
+++ Posh-IBWAPI/Posh-IBWAPI.psm1
@@ -22,6 +22,26 @@ if (-not ([System.Management.Automation.PSTypeName]'CertValidation').Type)
 "@
 }
 
+# In all of the PowerShell environments tested so far, the set of supported TLS protocols
+# configured by default in .NET seem to only include SSLv3 and TLSv1.0. So even if .NET
+# supports using things like TLS 1.1 or 1.2, cmdlets like Invoke-RestMethod will be limited
+# to TLS 1.0 unless the setting is overridden (per session).
+#
+# NIOS's default config supports TLS 1.0+, but since 8.x it's now possible to explicitly set
+# supported TLS types. So if an environment disables TLS 1.0 support, Invoke-RestMethod fails
+# with the following error:
+# "The underlying connection was closed: An unexpected error occurred on a send."
+#
+# To give users a more secure default when using this module and try to prevent potential errors
+# when running against grids who have customized their supported protocols, we will change
+# the default set of protocols to include all protocol types beyond 1.0 (or currently configured
+# max level) supported in the current installed .NET framework.
+$currentMaxTls = [Math]::Max([Net.ServicePointManager]::SecurityProtocol.value__,[Net.SecurityProtocolType]::Tls.value__)
+$newTlsTypes = [enum]::GetValues('Net.SecurityProtocolType') | ?{ $_ -gt $currentMaxTls }
+$newTlsTypes | %{
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+}
+
 # initialize the config container related stuff
 $script:CurrentHost = [string]::Empty
 if (!$script:Config) { $script:Config = @{} }
