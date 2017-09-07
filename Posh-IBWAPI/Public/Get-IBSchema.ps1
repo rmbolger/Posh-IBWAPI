41bfec28ed83bd6eef30d9d91719da7278f8389c
Ryan Bolger <z97y9drumh@snkmail.com>
2017-09-07 00:25:46 -0700

    added -NoFields and -NoFunctions. removed pretty printing colors. minor fixes

--- Posh-IBWAPI/Public/Get-IBSchema.ps1
+++ Posh-IBWAPI/Public/Get-IBSchema.ps1
@@ -6,7 +6,9 @@ function Get-IBSchema {
         [switch]$Raw,
         [string[]]$Fields,
         [string[]]$Operations,
+        [switch]$NoFields,
         [string[]]$Functions,
+        [switch]$NoFunctions,
         [Alias('host')]
         [string]$WAPIHost,
         [Alias('version')]
@@ -46,7 +48,7 @@ function Get-IBSchema {
 
     # cache some base schema stuff that we'll potentially need later
     if (!$cfg.SupportedVersions -or !$cfg[$WAPIVersion]) {
-        $schema = Invoke-IBWAPI -Uri "$APIBase/?_schema" @opts
+        $schema = Invoke-IBWAPI -Uri "$($APIBase)?_schema" @opts
 
         # set supported versions
         $cfg.SupportedVersions = $schema.supported_versions | Sort-Object @{E={[Version]$_}}
@@ -65,10 +67,6 @@ function Get-IBSchema {
         return
     }
 
-    # prep some stuff we'll need for pretty printing
-    $prettyColors = @{ForegroundColor='Cyan';BackgroundColor='Black'}
-    $consoleWidth = $host.ui.RawUI.maxWindowSize.Width - 1
-
     if (![String]::IsNullOrWhiteSpace($ObjectType)) {
         # We want to support wildcard searches and partial matching on object types.
         Write-Verbose "ObjectType: $ObjectType"
@@ -78,27 +76,30 @@ function Get-IBSchema {
             # multiple matches
             $message = "Multiple object matches found for $($ObjectType)"
             if ($Raw) { throw $message }
-            "$($message):" | Word-Wrap -Pad | Write-Host @prettyColors
-            $objMatches | %{ $_ | Word-Wrap -Pad | Write-Host @prettyColors }
+            "$($message):" | Word-Wrap -Pad | Write-Host
+            $objMatches | %{ $_ | Word-Wrap -Pad | Write-Host }
             return
         }
         elseif ($objMatches.count -eq 0 ) {
+            Write-Verbose "Retrying matches with implied wildcards"
             # retry matching with implied wildcards
             $objMatches = $cfg[$WAPIVersion] | %{ if ($_ -like "*$ObjectType*") { $_ } }
             if ($objMatches.count -gt 1) {
                 # multiple matches
                 $message = "Multiple object matches found for $($ObjectType)"
                 if ($Raw) { throw $message }
-                "$($message):" | Word-Wrap -Pad | Write-Host @prettyColors
-                $objMatches | %{ $_ | Word-Wrap -Pad | Write-Host @prettyColors }
+                "$($message):" | Word-Wrap -Pad | Write-Host
+                $objMatches | %{ $_ | Word-Wrap -Pad | Write-Host }
                 return
             }
             elseif ($objMatches.count -eq 0) {
                 # no matches, even with wildcards
                 $message = "No matches found for $($ObjectType)"
                 if ($Raw) { throw $message }
-                else { $message | Word-Wrap -Pad | Write-Debug @prettyColors }
+                else { $message | Word-Wrap -Pad | Write-Warning }
                 return
+            } else {
+                $ObjectType = $objMatches
             }
         }
         else {
@@ -125,7 +126,7 @@ function Get-IBSchema {
     # return the schema object directly, if asked
     if ($Raw) { return $schema }
 
-    function BlankLine() { ' ' | Word-Wrap -Pad | Write-Host @prettyColors }
+    function BlankLine() { ' ' | Word-Wrap -Pad | Write-Host }
     function PrettifySupports([string]$supports) {
         # The 'supports' property of a schema Field is a lower cases string
         # containing one or more of r,w,u,c,d for the supported operations of
@@ -149,15 +150,15 @@ function Get-IBSchema {
     if (!$schema.type) {
         # base schema object
         BlankLine
-        "Requested Version: $($schema.requested_version)" | Word-Wrap -Pad | Write-Host @prettyColors
+        "Requested Version: $($schema.requested_version)" | Word-Wrap -Pad | Write-Host
         BlankLine
-        "Supported Versions:" | Word-Wrap -Pad | Write-Host @prettyColors
+        "Supported Versions:" | Word-Wrap -Pad | Write-Host
         BlankLine
-        $schema | Select-Object -expand supported_versions | Format-Columns -prop {$_} -col 4 @prettyColors
+        $schema | Select-Object -expand supported_versions | Format-Columns -prop {$_} -col 4
         BlankLine
-        "Supported Objects:" | Word-Wrap -Pad | Write-Host @prettyColors
+        "Supported Objects:" | Word-Wrap -Pad | Write-Host
         BlankLine
-        $schema | Select-Object -expand supported_objects | Format-Columns -prop {$_} @prettyColors
+        $schema | Select-Object -expand supported_objects | Format-Columns -prop {$_}
         BlankLine
     }
     else {
@@ -169,20 +170,20 @@ function Get-IBSchema {
         # object type schema
         $typeStr = "$($schema.type) (WAPI $($schema.version))"
         BlankLine
-        'OBJECT' | Word-Wrap -Pad | Write-Host @prettyColors
-        $typeStr | Word-Wrap -Pad -Indent 4 | Write-Host @prettyColors
+        'OBJECT' | Word-Wrap -Pad | Write-Host
+        $typeStr | Word-Wrap -Pad -Indent 4 | Write-Host
         if ($schema.restrictions) {
             BlankLine
-            'RESTRICTIONS' | Word-Wrap -Pad | Write-Host @prettyColors
-            "$($schema.restrictions -join ', ')" | Word-Wrap -Pad -Indent 4 | Write-Host @prettyColors
+            'RESTRICTIONS' | Word-Wrap -Pad | Write-Host
+            "$($schema.restrictions -join ', ')" | Word-Wrap -Pad -Indent 4 | Write-Host
         }
         if ($schema.cloud_additional_restrictions) {
             BlankLine
-            'CLOUD RESTRICTIONS' | Word-Wrap -Pad | Write-Host @prettyColors
-            "$($schema.cloud_additional_restrictions -join ', ')" | Word-Wrap -Pad -Indent 4 | Write-Host @prettyColors
+            'CLOUD RESTRICTIONS' | Word-Wrap -Pad | Write-Host
+            "$($schema.cloud_additional_restrictions -join ', ')" | Word-Wrap -Pad -Indent 4 | Write-Host
         }
 
-        if ($schema.fields.count -gt 0) {
+        if ($schema.fields.count -gt 0 -and !$NoFields) {
             # get the length of the longest field name so we can make sure not to truncate that column
             $nameMax = [Math]::Max(($schema.fields.name | sort -desc @{E={$_.length}} | select -first 1).length + 1, 6)
             # get the length of the longest type name (including potential array brackets) so we can
@@ -191,8 +192,8 @@ function Get-IBSchema {
 
             $format = "{0,-$nameMax}{1,-$typeMax}{2,-9}{3,-5}{4,-6}"
             BlankLine
-            ($format -f 'Field','Type','Supports','Base','Search') | Word-Wrap -Pad | Write-Host @prettyColors
-            ($format -f '-----','----','--------','----','------') | Word-Wrap -Pad | Write-Host @prettyColors
+            ($format -f 'Field','Type','Supports','Base','Search') | Word-Wrap -Pad | Write-Host
+            ($format -f '-----','----','--------','----','------') | Word-Wrap -Pad | Write-Host
 
             # sort base fields first, then alphabetical with the rest
             $schema.fields | sort @{E='standard_field';Desc=$true},@{E='name';Desc=$false} | %{
@@ -226,81 +227,82 @@ function Get-IBSchema {
 
                 # there should always be at least one type, so write that with the rest of
                 # the table values
-                ($format -f $_.name,$_.type[0],(PrettifySupports $_.supports),$base,$_.searchable_by) | Word-Wrap -Pad | Write-Host @prettyColors
+                ($format -f $_.name,$_.type[0],(PrettifySupports $_.supports),$base,$_.searchable_by) | Word-Wrap -Pad | Write-Host
 
                 # write additional types on their own line
                 if ($_.type.count -gt 1) {
                     for ($i=1; $i -lt $_.type.count; $i++) {
-                        "$(''.PadRight($nameMax))$($_.type[$i])" | Word-Wrap -Pad | Write-Host @prettyColors
+                        "$(''.PadRight($nameMax))$($_.type[$i])" | Word-Wrap -Pad | Write-Host
                     }
                 }
             }
         }
 
-        if ($funcs.count -gt 0) {
-            BlankLine
-            "FUNCTIONS" | Word-Wrap -Pad | Write-Host @prettyColors
-        }
-        $funcs | %{
-
-            if ($Functions.count -le 0) {
-                $funcStr = "$($_.name)($($_.schema.input_fields.name -join ', '))"
-                if ($_.schema.output_fields.count -gt 0) {
-                    $funcStr += " => $($_.schema.output_fields.name -join ', ')"
-                }
-                $funcStr | Word-Wrap -Indent 4 -Pad | Write-Host @prettyColors
-            }
-            else {
-                $name = $_.name
-
-                # skip functions that weren't specified in the list
-                if (($Functions | %{ $name -like "*$_*" }) -notcontains $true) {
-                    return
-                }
-
+        if (!$NoFunctions) {
+            if ($funcs.count -gt 0) {
                 BlankLine
-                '    ----------------------------------------------------------' | Word-Wrap -Pad | Write-Host @prettyColors
-                $_.name | Word-Wrap -Indent 4 -Pad | Write-Host @prettyColors
-                '    ----------------------------------------------------------' | Word-Wrap -Pad | Write-Host @prettyColors
-                if ($_.doc) {
-                    $_.doc | Word-Wrap -Indent 8 -Pad | Write-Host @prettyColors
-                }
-                if ($_.schema.input_fields.count -gt 0) {
-                    BlankLine
-                    "Input fields" | Word-Wrap -Indent 4 -Pad | Write-Host @prettyColors
-                    foreach ($field in $_.schema.input_fields) {
-                        if ($field.enum_values) {
-                            $type = $field.enum_values -join '|'
-                        } else {
-                            $type = $field.type -join '|'
-                        }
-                        if ($field.is_array) { $type = "[$type]" }
-                        BlankLine
-                        "$($field.name) - $type" | Word-Wrap -Indent 8 -Pad | Write-Host @prettyColors
-                        $field.doc | Word-Wrap -Indent 12 -Pad | Write-Host @prettyColors
-                    }
-                }
-                if ($_.schema.output_fields.count -gt 0) {
-                    BlankLine
-                    "Output fields" | Word-Wrap -Indent 4 -Pad | Write-Host @prettyColors
-                    foreach ($field in $_.schema.input_fields) {
-                        if ($field.enum_values) {
-                            $type = $field.enum_values -join '|'
-                        } else {
-                            $type = $field.type -join '|'
-                        }
-                        if ($field.is_array) { $type = "[$type]" }
-                        BlankLine
-                        "$($field.name) - $type" | Word-Wrap -Indent 8 -Pad | Write-Host @prettyColors
-                        $field.doc | Word-Wrap -Indent 12 -Pad | Write-Host @prettyColors
-                    }
-                }
+                "FUNCTIONS" | Word-Wrap -Pad | Write-Host
             }
+            $funcs | %{
 
-        } # end $funcs loop
+                if ($Functions.count -le 0) {
+                    $funcStr = "$($_.name)($($_.schema.input_fields.name -join ', '))"
+                    if ($_.schema.output_fields.count -gt 0) {
+                        $funcStr += " => $($_.schema.output_fields.name -join ', ')"
+                    }
+                    $funcStr | Word-Wrap -Indent 4 -Pad | Write-Host
+                }
+                else {
+                    $name = $_.name
 
+                    # skip functions that weren't specified in the list
+                    if (($Functions | %{ $name -like "*$_*" }) -notcontains $true) {
+                        return
+                    }
 
+                    BlankLine
+                    '    ----------------------------------------------------------' | Word-Wrap -Pad | Write-Host
+                    $_.name | Word-Wrap -Indent 4 -Pad | Write-Host
+                    '    ----------------------------------------------------------' | Word-Wrap -Pad | Write-Host
+                    if ($_.doc) {
+                        $_.doc | Word-Wrap -Indent 8 -Pad | Write-Host
+                    }
+                    if ($_.schema.input_fields.count -gt 0) {
+                        BlankLine
+                        "Input fields" | Word-Wrap -Indent 4 -Pad | Write-Host
+                        foreach ($field in $_.schema.input_fields) {
+                            if ($field.enum_values) {
+                                $type = $field.enum_values -join '|'
+                            } else {
+                                $type = $field.type -join '|'
+                            }
+                            if ($field.is_array) { $type = "[$type]" }
+                            BlankLine
+                            "$($field.name) - $type" | Word-Wrap -Indent 8 -Pad | Write-Host
+                            $field.doc | Word-Wrap -Indent 12 -Pad | Write-Host
+                        }
+                    }
+                    if ($_.schema.output_fields.count -gt 0) {
+                        BlankLine
+                        "Output fields" | Word-Wrap -Indent 4 -Pad | Write-Host
+                        foreach ($field in $_.schema.output_fields) {
+                            if ($field.enum_values) {
+                                $type = $field.enum_values -join '|'
+                            } else {
+                                $type = $field.type -join '|'
+                            }
+                            if ($field.is_array) { $type = "[$type]" }
+                            BlankLine
+                            "$($field.name) - $type" | Word-Wrap -Indent 8 -Pad | Write-Host
+                            $field.doc | Word-Wrap -Indent 12 -Pad | Write-Host
+                        }
+                    }
+                }
 
+            } # end $funcs loop
+        }
+
+        BlankLine
     }
 
 
@@ -311,19 +313,28 @@ function Get-IBSchema {
         Query the schema of an object or the base appliance.
 
     .DESCRIPTION
-        Without any parameters, this function will return the base appliance schema object which among other things include the list of supported WAPI versions and object types. Providing an -ObjectType will return the schema object for that type which among other things includes the list of supported fields and how they may be queried.
+        Without any parameters, this function will return the base appliance schema object which includes the list of supported WAPI versions and object types. Providing an -ObjectType will return the schema object for that type which includes a list of supported fields and functions.
 
     .PARAMETER ObjectType
-        Object type string. (e.g. network, record:host, range)
+        Object type string. (e.g. network, record:host, range). Partial names and wildcards are supported. If the ObjectType parameter would match multiple objects, the list of matching objects will be returned.
 
     .PARAMETER Raw
         If set, the schema object will be returned as-is rather than pretty printing the output. All parameters except -ObjectType will be ignored.
 
     .PARAMETER Fields
-        A list of Field names to include in the output. Wildcards are supported. If not specified, all Fields will be included.
+        A list of Field names to include in the output. Wildcards are supported. This parameter is ignored if -NoFields is specified. If neither is specified, all Fields will be included.
 
     .PARAMETER Operations
         A list of supported operation codes: r (read), w (write/create), u (update/set), s (search), d (delete). Only the Fields supporting at least one of these operations will be included in the output. If not specified, all Fields will be included.
+
+    .PARAMETER NoFields
+        If set, the object's fields will not be included in the output.
+
+    .PARAMETER Functions
+        A list of Function names to include in the output. Wildcards are supported. This parameter is ignored if -NoFunctions is specified. If neither is specified, all Functions will be included.
+
+    .PARAMETER NoFunctions
+        If set, the object's functions will not be included in the output.
 
     .PARAMETER WAPIHost
         The fully qualified DNS name or IP address of the Infoblox WAPI endpoint (usually the grid master). This parameter is required if not already set using Set-IBWAPIConfig.
@@ -346,12 +357,27 @@ function Get-IBSchema {
     .EXAMPLE
         Get-IBSchema
 
-        Get the root schema object
+        Get the root schema object.
 
     .EXAMPLE
         Get-IBSchema record:host
 
-        Get the schema object for the record:host type.
+        Get the record:host schema object.
+
+    .EXAMPLE
+        Get-IBSchema record:host -Raw
+
+        Get the record:host schema object in raw object form.
+
+    .EXAMPLE
+        Get-IBSchema grid -Fields enable*,name
+
+        Get the grid schema object and only include the name field and fields that start with 'enable'.
+
+    .EXAMPLE
+        Get-IBSchema network -Operations s -NoFunctions
+
+        Get the network schema object and only include fields that are searchable and skip function definitions.
 
     .LINK
         Project: https://github.com/rmbolger/Posh-IBWAPI
