7bc549429b8d70622e3c96c42055d9f8b59dbfa6
Ryan Bolger <z97y9drumh@snkmail.com>
2018-06-28 09:36:21 -0700

    better fix for Github requiring TLS 1.2

--- instdev.ps1
+++ instdev.ps1
@@ -15,13 +15,7 @@ if ('PSEdition' -notin $PSVersionTable.Keys -or $PSVersionTable.PSEdition -eq 'D
 New-Item -ItemType Directory -Force -Path $installpath | out-null
 
 if ([String]::IsNullOrWhiteSpace($PSScriptRoot)) {
-    # likely running from online, so download and extract
-    $webclient = New-Object System.Net.WebClient
-    $url = 'https://github.com/rmbolger/Posh-IBWAPI/archive/master.zip'
-    Write-Host "Downloading latest version of Posh-IBWAPI from $url" -ForegroundColor Cyan
-    $file = Join-Path ([system.io.path]::GetTempPath()) 'Posh-IBWAPI.zip'
-    $webclient.DownloadFile($url,$file)
-    Write-Host "File saved to $file" -ForegroundColor Green
+    # likely running from online
 
     # GitHub now requires TLS 1.2
     # https://blog.github.com/2018-02-23-weak-cryptographic-standards-removed/
@@ -30,6 +24,14 @@ if ([String]::IsNullOrWhiteSpace($PSScriptRoot)) {
     $newTlsTypes | ForEach-Object {
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
     }
+
+    # download and extract
+    $webclient = New-Object System.Net.WebClient
+    $url = 'https://github.com/rmbolger/Posh-IBWAPI/archive/master.zip'
+    Write-Host "Downloading latest version of Posh-IBWAPI from $url" -ForegroundColor Cyan
+    $file = Join-Path ([system.io.path]::GetTempPath()) 'Posh-IBWAPI.zip'
+    $webclient.DownloadFile($url,$file)
+    Write-Host "File saved to $file" -ForegroundColor Green
 
     # try to use Expand-Archive if it exists, otherwise assume Desktop
     # edition and use COM
