1de5e9fe78349da38d2a57bf0380cbb2b484e64f
Ryan Bolger <z97y9drumh@snkmail.com>
2019-04-04 08:41:05 -0700

    added .NET version check now that we require 4.5+

--- Posh-IBWAPI/Posh-IBWAPI.psm1
+++ Posh-IBWAPI/Posh-IBWAPI.psm1
@@ -1,5 +1,22 @@
 #Requires -Version 3.0
 
+# Before we do anything else, make sure we have a sufficient .NET version that supports
+# System.Net.Http.MultipartFormDataContent for the file upload cmdlets.
+# Any version of .NET Core should already work.
+if (-not $PSEdition -or $PSEdition -eq 'Desktop') {
+    # https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#to-check-for-a-minimum-required-net-framework-version-by-querying-the-registry-in-powershell-net-framework-45-and-later
+    $netBuild = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release
+    if ($netBuild -ge 378389) { <# 4.5+ - all good #> }
+    else {
+        # versions prior to 4.5 don't actually have the Release build number in the registry
+        # so just display whatever the CLRVersion reports.
+        Write-Warning "**********************************************************************"
+        Write-Warning "Insufficient .NET version. Found .NET $($PSVersionTable.CLRVersion.ToString())."
+        Write-Warning ".NET 4.5 or later is required to ensure proper functionality."
+        Write-Warning "**********************************************************************"
+    }
+}
+
 # Set the persistent config file path based on edition/platform
 if ('PSEdition' -notin $PSVersionTable.Keys -or $PSVersionTable.PSEdition -eq 'Desktop' -or $IsWindows) {
     $script:ConfigFolder = $env:LOCALAPPDATA
