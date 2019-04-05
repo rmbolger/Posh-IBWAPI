7ac66176e28b2a71046b286d49bd0cab3f307111
Ryan Bolger <z97y9drumh@snkmail.com>
2019-04-05 14:36:50 -0700

    made OutFile mandatory and added help docs

--- Posh-IBWAPI/Public/Receive-IBFile.ps1
+++ Posh-IBWAPI/Public/Receive-IBFile.ps1
@@ -4,9 +4,10 @@ function Receive-IBFile {
         [Parameter(Mandatory=$True)]
         [Alias('name')]
         [string]$FunctionName,
+        [Parameter(Mandatory=$True)]
+        [string]$OutFile,
         [Alias('args')]
         [PSObject]$FunctionArgs,
-        [string]$OutFile,
         [Alias('host')]
         [string]$WAPIHost,
         [Alias('version')]
@@ -42,4 +43,56 @@ function Receive-IBFile {
         }
 
     }
+
+
+
+
+    <#
+    .SYNOPSIS
+        Download a file from a fileop function
+
+    .DESCRIPTION
+        This is a wrapper around the various fileop functions that allow data export from Infoblox.
+
+    .PARAMETER FunctionName
+        The name of the fileop download function to call.
+
+    .PARAMETER OutFile
+        Specifies the output file that this cmdlet saves the response body. Enter a path and file name. If you omit the path, the default is the current location.
+
+    .PARAMETER FunctionArgs
+        An object with the required parameters for the function.
+
+    .PARAMETER WAPIHost
+        The fully qualified DNS name or IP address of the Infoblox WAPI endpoint (usually the grid master). This parameter is required if not already set using Set-IBWAPIConfig.
+
+    .PARAMETER WAPIVersion
+        The version of the Infoblox WAPI to make calls against (e.g. '2.2').
+
+    .PARAMETER Credential
+        Username and password for the Infoblox appliance. This parameter is required unless -WebSession is specified or was already set using Set-IBWAPIConfig.
+
+    .PARAMETER WebSession
+        A WebRequestSession object returned by Get-IBSession or set when using Invoke-IBWAPI with the -SessionVariable parameter. This parameter is required unless -Credential is specified or was already set using Set-IBWAPIConfig.
+
+    .PARAMETER IgnoreCertificateValidation
+        If set, SSL/TLS certificate validation will be disabled. Overrides value stored with Set-IBWAPIConfig.
+
+    .EXAMPLE
+        Receive-IBFile getgriddata .\backup.tar.gz -args @{type='BACKUP'}
+
+        Download a grid backup file using the 'getgriddata' fileop function.
+
+    .EXAMPLE
+        Receive-IBFile csv_export .\host-records.csv -args @{_object='record:host'}
+
+        Download a CSV export of all host records using the 'csv_export' fileop function.
+
+    .LINK
+        Project: https://github.com/rmbolger/Posh-IBWAPI
+
+    .LINK
+        Invoke-IBFunction
+
+    #>
 }
